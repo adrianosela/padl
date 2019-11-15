be2c1597f3c90c39e8aa9d23afeed4d2b2249770
Adriano <adriano.selaviles@gmail.com>
2019-11-15 14:06:05 -0800

    omit empty version from healthcheck endpoint

--- api/payloads/healthcheck.go
+++ api/payloads/healthcheck.go
@@ -2,6 +2,6 @@ package payloads
 
 // HealthcheckResponse contains server health data
 type HealthcheckResponse struct {
-	Version    string `json:"version"`
+	Version    string `json:"version,omitempty"`
 	DeployTime string `json:"deployed_at"`
 }
