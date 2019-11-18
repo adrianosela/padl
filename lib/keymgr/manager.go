75735a035e8f8b7e88883f835ad4cfabdbf8cbe7
Adriano <adriano.selaviles@gmail.com>
2019-11-18 00:20:10 -0800

    fix keymgr interface

--- lib/keymgr/manager.go
+++ lib/keymgr/manager.go
@@ -3,7 +3,7 @@ package keymgr
 // Manager represents key management operations
 type Manager interface {
 	PutPriv(string, string) error
-	GetPriv(string) error
+	GetPriv(string) (string, error)
 	PutPub(string, string) error
-	GetPub(string) error
+	GetPub(string) (string, error)
 }
--- lib/padlfile/decrypt.go
+++
@@ -1,3 +0,0 @@
-package padlfile
-
-// TODO: decrypt padlfile secrets
