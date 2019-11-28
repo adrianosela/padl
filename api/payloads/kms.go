113db7e43cb4dbc511521c9779e52a292f9ce923
Christian Neufeld <christian.neufeld@alumni.ubc.ca>
2019-11-27 20:01:28 -0800

    fixed typos

--- api/payloads/kms.go
+++ api/payloads/kms.go
@@ -7,12 +7,12 @@ type DecryptSecretRequest struct {
 	Secret string `json:"secret"`
 }
 
-// DecryptSecretResponse contains reponse message to a secret decyrption request
+// DecryptSecretResponse contains reponse message to a secret decryption request
 type DecryptSecretResponse struct {
 	Message string `json:"message"`
 }
 
-// Validate validates a secret decrytion request
+// Validate validates a secret decryption request
 func (a *DecryptSecretRequest) Validate() error {
 	if a.Secret == "" {
 		return errors.New("no secret provided")
